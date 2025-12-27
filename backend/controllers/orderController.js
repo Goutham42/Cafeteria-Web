c3967cc33b1931b7637ee2d8a5fd8605487c9c4d
Goutham <gouthamhavoc@gmail.com>
2025-12-27 16:27:54 +0530

    Fix order controller exports and route imports

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,10 +1,13 @@
-import Order from "../models/Order.js";
-
-export const placeOrder = async (req, res) => {
+export const createOrder = async (req, res) => {
   const order = await Order.create({
     user: req.user.id,
     items: req.body.items,
     totalPrice: req.body.totalPrice
   });
   res.json(order);
+};
+
+export const getOrders = async (req, res) => {
+  const orders = await Order.find();
+  res.json(orders);
 };
